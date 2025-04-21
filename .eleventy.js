ed8d676244b4e2497bf4ce9ef92b9a41aafd8de7
frontrabotka <frazrabotka@gmail.com>
2025-04-21 12:02:22 +0300

    Первоначальная настройка проекта

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,48 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { minify } = require("html-minifier-terser");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/assets');
-    eleventyConfig.addPassthroughCopy('src/CNAME'); // Для кастомного домена
-    eleventyConfig.addPassthroughCopy('src/.nojekyll'); // Для GitHub Pages
-
+    eleventyConfig.addPassthroughCopy('src/CNAME');
+    eleventyConfig.addPassthroughCopy('src/.nojekyll');
     eleventyConfig.addPlugin(syntaxHighlight);
 
-    eleventyConfig.env.process();
+    // Убираем CSS из templateFormats, так как мы обрабатываем его через addExtension
+    eleventyConfig.setTemplateFormats(['html', 'njk', 'md']);
+
+    eleventyConfig.addExtension('css', {
+        outputFileExtension: 'css',
+        compile: async (content, path) => {
+            if (path.includes('src/assets/style')) {
+                return await minify(content, {
+                    minifyCSS: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                });
+            }
+            return content;
+        }
+    });
+
+    eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+        if (outputPath.endsWith(".html")) {
+            return minify(content, {
+                collapseWhitespace: true,
+                removeComments: true,
+                minifyJS: true,
+                minifyCSS: true,
+                removeEmptyAttributes: true,
+                removeEmpty: true,
+                removeOptionalTags: true,
+                removeRedundantAttributes: true
+            });
+        }
+        return content;
+    });
 
     return {
         htmlTemplateEngine: 'njk',
         markdownTemplateEngine: 'njk',
-        templateFormats: ['html', 'njk', 'md'],
         dir: {
             input: 'src',
             output: 'site',
