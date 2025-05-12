d8da12edf459bd4f66af45c03797279ecac65c15
frontrabotka <frazrabotka@gmail.com>
2025-05-12 16:29:01 +0300

    Plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,45 +1,24 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const { minify } = require("html-minifier-terser");
+const eleventyPluginFilesMinifier = require("@codestitchofficial/eleventy-plugin-minify");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/assets');
     eleventyConfig.addPassthroughCopy('.nojekyll');
     eleventyConfig.addPassthroughCopy('robots.txt');
     eleventyConfig.addPassthroughCopy('src/sitemap.xml');
     eleventyConfig.addPassthroughCopy('manifest.webmanifest');
     eleventyConfig.addPassthroughCopy('src/scripts/**/*');
-    eleventyConfig.addPlugin(syntaxHighlight);
 
     eleventyConfig.setTemplateFormats(['html', 'njk', 'md']);
 
-    eleventyConfig.addExtension('css', {
-        outputFileExtension: 'css',
-        compile: async (content, path) => {
-            if (path.includes('src/assets/style')) {
-                return await minify(content, {
-                    minifyCSS: true,
-                    collapseWhitespace: true,
-                    removeComments: true
-                });
-            }
-            return content;
+    eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+    eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+    eleventyConfig.addPlugin(syntaxHighlight, {
+        templateFormats: ["njk", "md"],
+        lineSeparator: "\n",
+        preAttributes: {
+            tabindex: 0
         }
-    });
-
-    eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
-        if (outputPath.endsWith(".html")) {
-            return minify(content, {
-                collapseWhitespace: true,
-                removeComments: true,
-                minifyJS: true,
-                minifyCSS: true,
-                removeEmptyAttributes: true,
-                removeEmpty: true,
-                removeOptionalTags: true,
-                removeRedundantAttributes: true
-            });
-        }
-        return content;
     });
 
     return {
@@ -51,9 +30,5 @@ module.exports = function(eleventyConfig) {
             includes: 'includes'
         },
         pathPrefix: '/conspect',
-        quiet: false,
-        data: {
-            site: {}
-        }
     };
 };
